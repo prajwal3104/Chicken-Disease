0464e42b01cc0c84f5364c014eda6720aece5e3f
prajwal3104 <Prajwal0836@gmail.com>
2023-09-15 20:35:46 +0530

    data ingestion modular coding done

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("Hello World!") # This will print the message to the console and also to the log file.
+STAGE_NAME = "Stage 01: Data Ingestion Stage"
+try:
+    logger.info(f">>>>> stage name: {STAGE_NAME} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage name: {STAGE_NAME} completed!<<<<<")
+except Exception as e:
+    logger.error(f"stage name: {STAGE_NAME} error: {e}")
+    raise e
