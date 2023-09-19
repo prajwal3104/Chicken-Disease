0e179171e254bcb1a7b6c0615bdae4a6ca47720a
prajwal3104 <Prajwal0836@gmail.com>
2023-09-19 19:53:21 +0530

    Error in Prepare Base Model

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,18 +1,19 @@
 from cnnClassifier.constants import *
 import os
+import pathlib as Path
 from cnnClassifier.utils.common import read_yaml, create_directories
-from cnnClassifier.entity.config_entity import (DataIngestionConfig, 
-                                                PrepareCallbacksConfig)
+from cnnClassifier.entity.config_entity import (DataIngestionConfig,
+                                                PrepareBaseModelConfig, 
+                                                PrepareCallbacksConfig,
+                                                TrainingConfig)
 
 class ConfigurationManager:
     def __init__(
-        self,
+        self, 
         config_filepath = CONFIG_FILE_PATH,
         params_filepath = PARAMS_FILE_PATH):
-
         self.config = read_yaml(config_filepath)
         self.params = read_yaml(params_filepath)
-
         create_directories([self.config.artifacts_root])
 
 
@@ -32,12 +33,12 @@ class ConfigurationManager:
         return data_ingestion_config
 
 
-    def get_prepare_base_model_config(self) -> PrepareCallbacksConfig:
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
         config = self.config.prepare_base_model
         
         create_directories([config.root_dir])
 
-        prepare_base_model_config = PrepareCallbacksConfig(
+        prepare_base_model_config = PrepareBaseModelConfig(
             root_dir=Path(config.root_dir),
             base_model_path=Path(config.base_model_path),
             updated_base_model_path=Path(config.updated_base_model_path),
@@ -65,3 +66,25 @@ class ConfigurationManager:
         )
 
         return prepare_callback_config
+    
+    def get_training_config(self) -> TrainingConfig:
+        training = self.config.training
+        prepare_base_model = self.config.prepare_base_model
+        params = self.params
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chicken-fecal-images")
+        create_directories([
+            Path(training.root_dir)
+        ])
+
+        training_config = TrainingConfig(
+            root_dir=Path(training.root_dir),
+            trained_model_path=Path(training.trained_model_path),
+            updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+            training_data=Path(training_data),
+            params_epochs=params.EPOCHS,
+            params_batch_size=params.BATCH_SIZE,
+            params_is_augmentation=params.AUGMENTATION,
+            params_image_size=params.IMAGE_SIZE
+        )
+
+        return training_config
